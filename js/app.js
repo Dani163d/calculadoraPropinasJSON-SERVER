4199c76f1056ffefd043c483b3b3c94dad7fb2ad
Dani163d <Danielasilvacastillo163@gmail.com>
2024-10-05 20:32:14 -0400

    mostrando mas informacion de la API

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let cliente = {
     pedido: []
 };
 
+const categorias = {
+    1: "Comida",
+    2: "Bebida",
+    3: "Postres"
+}
+
 const btnGuarcarCliente = document.querySelector('#guardar-cliente');
 btnGuarcarCliente.addEventListener('click', guardarCliente);
 
@@ -67,13 +73,24 @@ function mostrarPlatillos(platillos) {
 
    platillos.forEach( platillo => {
     const row = document.createElement('DIV');
-    row.classList.add('row');
+    row.classList.add('row', 'py-3', 'border-top');
 
     const nombre = document.createElement('DIV');
     nombre.classList.add('col-md-4');
     nombre.textContent = platillo.nombre;
 
+    const precio = document.createElement('DIV');
+    precio.classList.add('col-md-3', 'fw-bold');
+    precio.textContent = `$${platillo.precio}`;
+
+    const categoria = document.createElement('DIV');
+    categoria.classList.add('col-md-3');
+    categoria.textContent = categorias [platillo.categoria];
+
     row.appendChild(nombre);
+    row.appendChild(precio);
+    row.appendChild(categoria);
+
     contenido.appendChild(row);
    } )
 }
