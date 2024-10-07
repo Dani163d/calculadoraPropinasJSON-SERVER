3bde58021030260c36709f4b7261c8f131def047
Dani163d <Danielasilvacastillo163@gmail.com>
2024-10-07 15:29:36 -0400

    limpiar el resumen al actualizar pedidos

--- js/app.js
+++ js/app.js
@@ -142,6 +142,10 @@ function agregarPlatillo(producto) {
         cliente.pedido = [...resultado]
     }
 
+    // limpiar el codigo html
+    limpiarHTML();
+
+    // mostrar el resumen
     actualizarResumen();
 }
 
@@ -149,7 +153,7 @@ function actualizarResumen() {
     const contenido = document.querySelector('#resumen .contenido');
 
     const resumen = document.createElement('DIV');
-    resumen.classList.add('col-md-6');
+    resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3', 'shadow');
 
     // informacion de la mesa
     const mesa = document.createElement('P');
@@ -173,8 +177,24 @@ function actualizarResumen() {
     mesa.appendChild(mesaSpan);
     hora.appendChild(horaSpan);
 
-    // aGREGAR EL CONTENIDO
-    contenido.appendChild(mesa);
-    contenido.appendChild(hora);
+    // titulo de la seccion
+    const heading = document.createElement('H3');
+    heading.textContent = 'Platillos Consumidos';
+    heading.classList.add('my-4', 'text-center')
 
+    // aGREGAR EL CONTENIDO
+    resumen.appendChild(mesa);
+    resumen.appendChild(hora);
+    resumen.appendChild(heading);
+
+    contenido.appendChild(resumen);
+
+}
+
+function limpiarHTML() {
+    const contenido = document.querySelector('#resumen .contenido')
+    
+    while (contenido.firstChild) {
+        contenido.removeChild(contenido.firstChild);
+    }
 }
